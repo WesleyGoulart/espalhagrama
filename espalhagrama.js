b68e665772d0fd2961050929107f0e32097bc777
Wesley Goulart <goulart89@gmail.com>
2023-12-12 15:59:02 -0300

    Valores sendo substituidos dentro do array

--- espalhagrama.js
+++ espalhagrama.js
@@ -1,6 +1,6 @@
 const grama = []
-let i = 20
-let p = 0
+let i = 5             // Tamanho do array
+let p = 0             // Índice do array
 
 if (i > 0) {
     while (i > 0) {
@@ -10,4 +10,8 @@ if (i > 0) {
     }
 }
 
+const sorteioGrama = grama.map(valor => valor + 1)         // Substitui todos os valores do array por 1
+
+
 console.log(grama)
+console.log(sorteioGrama)
