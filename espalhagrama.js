c7bdc97714143dc2f00687bba98ff7f08c944cf7
Wesley Goulart <goulart89@gmail.com>
2023-12-12 17:06:07 -0300

    Sorteando maior e menor valor

--- espalhagrama.js
+++ espalhagrama.js
@@ -1,25 +1,33 @@
-const grama = []
-let i = 3             // Tamanho do array
-let p = 0             // Índice do array
+const grama = []      
+let q = 3              // Tamanho do array
+let p = 0               // Índice do array
 
-function getRandomIntInclusive(p, i) {
+function getRandomIntInclusive(p, q) {                                      //Sorteio de um índice do array
     min = Math.ceil(p);
-    max = Math.floor(i);
+    max = Math.floor(q);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
-if (i > 0) {
+console.log(getRandomIntInclusive(p, q))
+
+if (q > 0) {
+    for (let i = q; i > 0; i = i - 1) {
+        grama[p] = 0
+        p++
+    }
+}
+
+/* if (i > 0) {
     while (i > 0) {
         grama[p] = 0
         p++
         i = i - 1
     }
 }
+*/
 
 
 const sorteioGrama = grama.map(valor => valor + 1)         // Substitui todos os valores do array por 1
 
-
-console.log(grama)
 console.log(sorteioGrama)
-console.log(getRandomIntInclusive(p, i))
+console.log(grama)
