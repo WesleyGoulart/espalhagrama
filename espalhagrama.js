2310048568768b3073767dee530ec2e5645aaa19
Wesley Goulart <goulart89@gmail.com>
2023-12-12 16:24:55 -0300

    Sorteio ainda não sorteando maior e menor valor

--- espalhagrama.js
+++ espalhagrama.js
@@ -1,6 +1,12 @@
 const grama = []
-let i = 5             // Tamanho do array
+let i = 3             // Tamanho do array
 let p = 0             // Índice do array
+
+function getRandomIntInclusive(p, i) {
+    min = Math.ceil(p);
+    max = Math.floor(i);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
 if (i > 0) {
     while (i > 0) {
@@ -10,8 +16,10 @@ if (i > 0) {
     }
 }
 
+
 const sorteioGrama = grama.map(valor => valor + 1)         // Substitui todos os valores do array por 1
 
 
 console.log(grama)
 console.log(sorteioGrama)
+console.log(getRandomIntInclusive(p, i))
